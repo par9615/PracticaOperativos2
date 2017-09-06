da741a03d65b8a304845fec188d5c14a4dfe1f6d
Miguel Hernández <par9615@gmail.com>
2017-09-05 19:28:39 -0500

    getty and init modified

--- init.c
+++ init.c
@@ -1,21 +1,31 @@
 #include<stdio.h>
 #include<unistd.h>
+#include<stdlib.h>
 #include<sys/wait.h>
+
+#define NPROC 6
 
 int main()
 {
 	int pid;
 	int status;
 
-	for(int i = 0; i < 6; i++)
+	for(int i = 0; i < NPROC; i++)
 	{
 		pid = fork();
 
-		if(pid == 0)
+		if(pid == 0)		
 			execlp("xterm", "xterm", "-e" , "./getty", NULL);
 	}
+	
 
-	wait(&status);
+	for(int i = 0; i < NPROC; i++)
+	{
+		wait(&status);
+		printf("juas --> %d\n", status>>8);
+	}
+
+	
 
 	return 0;
 }
