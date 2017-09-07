418b4654acff61ec483283c8432105a8b9ba1307
kireck211 <erickdesantiago11@gmail.com>
2017-09-06 21:36:54 -0500

    Removing own export

--- sh.c
+++ sh.c
@@ -6,87 +6,6 @@
 
 typedef enum { false, true } bool;
 
-typedef struct node
-{
-	char * key;
-	char * value;
-	struct node *left;
-	struct node *right;
-} Node;
-
-Node *create_node(char * key, char * value)
-{
-	Node * node = (Node *)malloc(sizeof(Node));
-	node->key = key;
-	node->value = value;
-	node->left = NULL;
-	node->right = NULL;
-
-	return node;
-}
-
-void insert(Node *parent, char * key, char * value)
-{
-	if (parent == NULL)
-	{
-		parent = create_node(key, value);
-		return;
-	}
-
-	int movement = strcmp(parent->key, key);
-	if (movement == 0)
-	{
-		parent->value = value;
-		return;
-	}
-	else if (movement < 0 && parent->left == NULL)
-	{
-		parent->left = create_node(key, value);
-		return;
-	}
-	else if (movement > 0 && parent->right == NULL)
-	{
-		parent->right = create_node(key, value);
-		return;
-	}
-
-	else if(movement > 0 && parent->right != NULL)
-		insert(parent->right, key, value);
-
-	else if (movement < 0 && parent->left != NULL)
-		insert(parent->left, key, value);
-}
-
-char *get_value_it(Node *parent, char * key)
-{
-	Node * actual = parent;
-	while(actual != NULL)
-	{
-		int movement = strcmp(actual->key, key);
-		if (movement == 0)
-			return actual->value;
-		else if (movement < 0)
-			actual = actual->left;
-		else
-			actual = actual->right;
-	}
-	return NULL;
-}
-
-char *get_value_re(Node * parent, char * key)
-{
-	if (parent == NULL)
-		return NULL;
-
-	int movement = strcmp(parent->key, key);
-	if (movement == 0)
-		return parent->value;
-	else if (movement < 0)
-		get_value_re(parent->left, key);
-	else
-		get_value_re(parent->right, key);
-}
-
 /* Returns an array of pointers with every pointer, pointing to a word
    and updates the value of size using a separator*/
 char **tokenize(char * string, int * size, char separator)
@@ -149,9 +68,6 @@ int main()
 	// array to read command string
 	char * command;
 
-	// Binay Tree of variables
-	Node * tree;
-
 	char c;
 	int i;
 
@@ -172,19 +88,7 @@ int main()
 		
 		// Creation of argv and update of argc
 		local_argv = tokenize(command, &local_argc, ' ');
-
-		if (strcmp(local_argv[0], "export") == 0)
-		{
-			int size;
-			char **parsed_key_value = tokenize(local_argv[1], &size, '=');
-			insert(tree, parsed_key_value[0], parsed_key_value[1]);
-		}
-		else if (strcmp(local_argv[0], "echo") == 0)
-		{
-			char * variable = &(local_argv[1][1]);
-			printf("%s\n", get_value_re(tree, variable));
-		}
-		else if (strcmp(local_argv[0], "exit") == 0)
+		if (strcmp(local_argv[0], "exit") == 0)
 		{
 			exit(0);
 		}
