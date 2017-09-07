a59ff94e6cfac315b0e9dd1e9e77293581610644
kireck211 <erickdesantiago11@gmail.com>
2017-09-06 22:37:07 -0500

    Fixed bugs

--- sh.c
+++ sh.c
@@ -6,6 +6,85 @@
 
 typedef enum { false, true } bool;
 
+char **tokenize(char *, int *, char);
+void free_memory(char **, int, char *);
+void create_shutdown_file();
+char **get_variable_and_value(char *);
+void create_shutdown_file();
+
+int main()
+{
+	// Argv and argc
+	char ** local_argv;
+	int local_argc = 0;
+
+	// array to read command string
+	char * command;
+
+	char c;
+	int i;
+
+	
+	while(c != EOF)
+	{
+		i = 0;
+		printf("sh >> ");
+
+		command = (char *)malloc(sizeof(char));
+
+		while((c = getchar()) != '\n')
+		{
+			command[i++] = c; // Adds the value of c to the array
+			command = realloc(command, i + 1); // Adds space for the next character
+		}
+		command[i] = '\0'; // character end string
+		
+		// Creation of argv and update of argc
+		local_argv = tokenize(command, &local_argc, ' ');
+		if (strcmp(local_argv[0], "exit") == 0)
+		{
+			exit(0);
+		}
+		else if (strcmp(local_argv[0], "shutdown") == 0)
+		{
+			create_shutdown_file();
+			exit(1);
+		}
+		else if (strcmp(local_argv[0], "export") == 0)
+		{
+			char ** variable_value = get_variable_and_value(local_argv[1]);
+			if (setenv(variable_value[0], variable_value[1], 1) == -1)
+			{
+				printf("PUTO EL QUE LO LEA\n");
+			}
+		}
+		else if (strcmp(local_argv[0], "echo") == 0)
+		{
+			char * temp = &(local_argv[1][1]);
+			char * value = getenv(temp);
+			if (value == NULL)
+				for(i = 1; i < local_argc; i++)
+					printf("%s\n", local_argv[i]);
+			else
+				printf("%s\n", getenv(temp));
+		}
+		else
+		{
+			int pid = fork(), status, status_child;
+			if (pid == 0) {
+				status_child = execvp(local_argv[0], local_argv);
+				if (status_child == -1)
+					printf("%s: command not found\n", local_argv[0]);
+			}
+			wait(&status);
+		}
+
+		free_memory(local_argv, local_argc, command);
+	}
+
+	return 0;
+}
+
 /* Returns an array of pointers with every pointer, pointing to a word
    and updates the value of size using a separator*/
 char **tokenize(char * string, int * size, char separator)
@@ -38,6 +117,7 @@ char **tokenize(char * string, int * size, char separator)
 	return array_pointers;
 }
 
+/* Free alocated memory assigned to pointers */
 void free_memory(char ** argv, int argc, char * command)
 {
 	int i;
@@ -48,6 +128,7 @@ void free_memory(char ** argv, int argc, char * command)
 	free(command);
 }
 
+/* Creates a file to comunicate to init program and close all terminals */
 void create_shutdown_file()
 {
 	FILE * file;
@@ -93,72 +174,4 @@ char **get_variable_and_value(char *string)
 	variable_and_value[0][last_i] = '\0';
 	variable_and_value[1][j] = '\0';
 	return variable_and_value;
-}
-
-int main()
-{
-	// Argv and argc
-	char ** local_argv;
-	int local_argc = 0;
-
-	// array to read command string
-	char * command;
-
-	char c;
-	int i;
-
-	
-	while(c != EOF)
-	{
-		i = 0;
-		printf("sh >> ");
-
-		command = (char *)malloc(sizeof(char));
-
-		while((c = getchar()) != '\n')
-		{
-			command[i++] = c; // Adds the value of c to the array
-			command = realloc(command, i + 1); // Adds space for the next character
-		}
-		command[i] = '\0'; // character end string
-		
-		// Creation of argv and update of argc
-		local_argv = tokenize(command, &local_argc, ' ');
-		if (strcmp(local_argv[0], "exit") == 0)
-		{
-			exit(0);
-		}
-		else if (strcmp(local_argv[0], "shutdown") == 0)
-		{
-			create_shutdown_file();
-			exit(0);
-		}
-		else if (strcmp(local_argv[0], "export") == 0)
-		{
-			char ** variable_value = get_variable_and_value(local_argv[1]);
-			if (setenv(variable_value[0], variable_value[1], 1) == -1)
-			{
-				printf("PUTO EL QUE LO LEA\n");
-			}
-		}
-		else if (strcmp(local_argv[0], "echo") == 0)
-		{
-			char * temp = &(local_argv[1][1]);
-			printf("%s\n", getenv(temp));
-		}
-		else
-		{
-			int pid = fork(), status, status_child;
-			if (pid == 0) {
-				status_child = execvp(local_argv[0], local_argv);
-				if (status_child == -1)
-					printf("%s: command not found\n", local_argv[0]);
-			}
-			wait(&status);
-		}
-
-		free_memory(local_argv, local_argc, command);
-	}
-
-	return 0;
 }
